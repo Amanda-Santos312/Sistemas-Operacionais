eaa6403ec752c82d314d37b5280bf20ff3c3fca5
Amanda Santos <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-06-07 11:55:07 -0300

    atividades

---
+++ thread.py
@@ -0,0 +1,40 @@
+import threading
+import time
+
+def tarefa1():
+    x = 0
+    
+    while x < 5:
+        print("Tarefa 1")
+        x += 1
+
+
+def tarefa2():
+    y = 0
+    
+    while y < 5: 
+       print("Tarefa 2")
+       y += 1
+     
+       
+def tarefa3():
+     z = 0
+     
+     while z < 5:
+         print("Tarefa 3")
+         z += 1
+           
+
+threading.Thread(target=tarefa1).start()
+tarefa2()
+tarefa3()
+
+
+
+'''threading.Thread(target=tarefa1).start() #chama a função
+tarefa2()'''
+
+'''
+O resultado vai ser uma impressão sem padrão, 
+pois ficará misturado de acordo com a necessidade do sistema.
+'''
